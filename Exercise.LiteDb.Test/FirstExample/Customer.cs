d7590466cece6aa30ae585fc316481202b775d74
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-12-18 20:40:03 +0100

    Init project

---
+++ Exercise.LiteDb.Test/FirstExample/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise.LiteDb.Test
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Phones { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+}
---
+++ Exercise.LiteDb.Test/FirstExample/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise.LiteDb.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Example()
+        {
+        }
+    }
+}

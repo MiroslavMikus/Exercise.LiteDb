e90777c49be019d675b759c15b954c4be46634c1
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-12-21 22:07:39 +0100

    Add document reference example

---
+++ Exercise.LiteDb.Test/ReferenceDocumentsExample/ReferenceDocumentTest.cs
@@ -0,0 +1,59 @@
+﻿using LiteDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Exercise.LiteDb.Test.ReferenceDocumentsExample
+{
+    [TestClass]
+    public class ReferenceDocumentTest
+    {
+        //public ReferenceDocumentTest()
+        //{
+        //    BsonMapper.Global
+        //        .Entity<Order>()
+        //        .DbRef(x => x.Customer, "customers");
+        //}
+
+        [TestMethod]
+        public void TestStoreReferences()
+        {
+            File.Delete("ThirdDatabase.db");
+
+            using (var db = new LiteRepository("ThirdDatabase.db"))
+            {
+                var customer = new Customer()
+                {
+                    Name = "Miro"
+                };
+
+                var order = new Order
+                {
+                    Customer = customer
+                };
+
+                db.Insert(customer, "customers");
+
+                db.Insert(order, "orders");
+            }
+        }
+
+        [TestMethod]
+        public void TestRestoreReferences()
+        {
+            using (var db = new LiteRepository("ThirdDatabase.db"))
+            {
+                var order = db.Query<Order>("orders")
+                    .Include(a => a.Customer)
+                    .FirstOrDefault();
+
+                Assert.IsNotNull(order.Customer);
+
+                Assert.AreEqual("Miro",order.Customer.Name);
+            }
+        }
+    }
+}

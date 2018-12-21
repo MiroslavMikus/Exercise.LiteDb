e18ccebca47bbe0a60383db8ed18504f4993e361
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-12-21 21:31:34 +0100

    Add complex customer test

---
+++ Exercise.LiteDb.Test/StoreComplexObjectExample/ComplexStoreTest.cs
@@ -0,0 +1,33 @@
+﻿using LiteDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise.LiteDb.Test.StoreComplexObjectExample
+{
+    [TestClass]
+    public class ComplexStoreTest
+    {
+        [TestMethod]
+        public void TestStore()
+        {
+            using (var db = new LiteDatabase(@"SecondDatabase.db"))
+            {
+                var customer = db.GetCollection<ComplexCustomer>("customers");
+
+                customer.Insert(new ComplexCustomer()
+                {
+                    Birthday = DateTime.Now,
+                    IsNotActive = true,
+                    Name = "Miro",
+                    Address = new Address
+                    {
+                        City = "Oberaudorf",
+                        Street = "Bahnhof"
+                    }
+                });
+            }
+        }
+    }
+}
